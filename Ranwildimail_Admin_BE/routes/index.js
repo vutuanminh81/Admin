2aa6d5b56c5d472714fa5973e8d7f584e41a1db3
vutuanminh81 <58020524+vutuanminh81@users.noreply.github.com>
2022-04-06 15:59:49 +0700

    commit

--- Ranwildimail_Admin_BE/routes/index.js
+++ Ranwildimail_Admin_BE/routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+var user = [
+  {
+    email: 'minhvt@gmail.com', password: 'admin'
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/Login', function (req, res) {
+  let result = user.find(user => user.email == req.body.email)
+
+  if (result) {
+    if (result.password == req.body.Password) {
+      res.status(200).send({
+        message: "oke"
+      })
+    } else {
+      res.status(200).send({
+        message: "not ok"
+      })
+    }
+  } else {
+    res.status(200).send({
+      message: "not okay"
+    })
+  }
+})
 
 module.exports = router;
