8b2c928ab126dfbb437b3d080f088f5c37dcd3e1
vutuanminh81 <58020524+vutuanminh81@users.noreply.github.com>
2022-04-07 16:42:18 +0700

    alo

--- Ranwildimail_Admin_BE/app.js
+++ Ranwildimail_Admin_BE/app.js
@@ -6,12 +6,15 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var wordRouter = require('./routes/word');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+
 
 app.use(function(req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
@@ -27,7 +30,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', usersRouter); 
+app.use('/word', wordRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ Ranwildimail_Admin_BE/config.js
@@ -0,0 +1,13 @@
+const firebase = require('firebase');
+const firebaseConfig= {
+    apiKey: "AIzaSyBPBBm9hMIgRCYXIF1eglTm1BMygUOkumY",
+    authDomain: "ranwildimal.firebaseapp.com",
+    projectId: "ranwildimal",
+    storageBucket: "ranwildimal.appspot.com",
+    messagingSenderId: "145722705403",
+    appId: "1:145722705403:web:78ee7502ec7dc2007eb75e"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const Word = db.collection('Word');
+module.exports = Word;
