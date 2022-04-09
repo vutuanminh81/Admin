ff3258ae109d254deb4c66ae8b881ecc85b7198f
vutuanminh81 <58020524+vutuanminh81@users.noreply.github.com>
2022-04-10 00:47:50 +0700

    cua minh

--- Ranwildimal_Admin_FE/src/App.js
+++ Ranwildimal_Admin_FE/src/App.js
@@ -2,11 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import LoginForm from './Layout/Login/LoginForm';
 
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
+import Navbar from './Component/navbar/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <LoginForm/>
+      <Navbar/>
+
+      {/* <Switch>
+        <Route path='/dashboard'>
+          <Dashboard/>
+        </Route>
+        <Route path='/word'>
+          <LoginForm/>
+        </Route>
+        <Route path='/'>
+          <LoginForm/>
+        </Route>
+      </Switch> */}
     </div>
   );
   
--- Ranwildimal_Admin_FE/src/Component/navbar/Navbar.js
+++ Ranwildimal_Admin_FE/src/Component/navbar/Navbar.js
@@ -1,38 +1,46 @@
 import React from 'react';
 import './style.css';
 
-function Navbar() {
-    return (
-        <nav className="pcoded-navbar">
-            <div className="navbar-wrapper">
-                <div className="navbar-brand header-logo">
-                    <a href="index.html" className="b-brand">
-                        <div className="b-bg">
-                            <i className="feather icon-trending-up"></i>
-                        </div>
-                        <span className="b-title">RANWILDIMAL</span>
-                    </a>
-                    
+import {
+    NavLink
+  } from "react-router-dom"
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <nav className="pcoded-navbar">
+                <div className="navbar-wrapper">
+                    <div className="navbar-brand header-logo">
+                        <a href="/dashboard" className="b-brand">
+                            <div className="b-bg">
+                                <i className="feather icon-trending-up"></i>
+                            </div>
+                            <span className="b-title">RANWILDIMAL</span>
+                        </a>
+
+                    </div>
+                    <div className="navbar-content scroll-div">
+                        <ul className="nav pcoded-inner-navbar">
+                            <li className="nav-item pcoded-menu-caption">
+                                <label>Word & Photograph</label>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/dashboard" className="nav-link" activeClassName="active"><span className="pcoded-micon"><i className="feather icon-home"></i></span><span className="pcoded-mtext">Dashboard</span></NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/words" className="nav-link" activeClassName="active"><span className="pcoded-micon"><i className="feather icon-sidebar"></i></span><span className="pcoded-mtext">Word & Photograph</span></NavLink></li>
+
+                            <li className="nav-item pcoded-menu-caption">
+                                <label>Other</label>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/logout" className="nav-link" activeClassName="active"><span className="pcoded-micon"><i className="feather icon-sidebar"></i></span><span className="pcoded-mtext">Logout</span></NavLink></li>
+                        </ul>
+                    </div>
                 </div>
-                <div className="navbar-content scroll-div">
-                    <ul className="nav pcoded-inner-navbar">
-                        <li className="nav-item pcoded-menu-caption">
-                            <label>Word & Photograph</label>
-                        </li>
-                        <li className="nav-item active">
-                            <a href="index.html" className="nav-link "><span className="pcoded-micon"><i className="feather icon-home"></i></span><span className="pcoded-mtext">Dashboard</span></a>
-                        </li>
-                        <li className="nav-item"><a href="sample-page.html" className="nav-link"><span className="pcoded-micon"><i className="feather icon-sidebar"></i></span><span className="pcoded-mtext">Word & Photograph</span></a></li>
-                        
-                        <li className="nav-item pcoded-menu-caption">
-                            <label>Other</label>
-                        </li>
-                        <li className="nav-item"><a href="sample-page.html" className="nav-link"><span className="pcoded-micon"><i className="feather icon-sidebar"></i></span><span className="pcoded-mtext">Logout</span></a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    )
+            </nav>
+        )
+    }
 }
 
 export default Navbar
